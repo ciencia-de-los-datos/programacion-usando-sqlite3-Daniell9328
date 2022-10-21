61c1f3373ed5e37033c4ca9a07e7ba99e2a34302
Daniell9328 <112450832+Daniell9328@users.noreply.github.com>
2022-10-21 09:55:41 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -17,22 +17,6 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-
-data_tbl1 = pd.read_csv(
-    "https://raw.githubusercontent.com/ciencia-de-los-datos/programacion-usando-sqlite3-Daniell9328/main/tbl1.csv",
-    sep = ",",
-    thousands = None,
-    decimal = ".",
-    encoding = "latin-1"
-)
-
-new_data_tbl1 = (data_tbl1.T.reset_index().T.reset_index(drop=True)
-            .set_axis(['K0', 'K1', 'c12', 'c13', 'c14', 'c15', 'c16'], axis='columns'))
-
-new_data_tbl1.to_sql(name= "tbl1", con = conn, if_exists = "replace")
-
-#cur.execute ("SELECT * FROM tbl1 LIMIT 1,3;").fetchall()
-
 cur.execute(
     """SELECT sum(c12)
        FROM tbl1
